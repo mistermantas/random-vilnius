11dd9916e59a9b8ded529c73d7434e8e47032253
AKSHAY_RAI <akshay11rai@gmail.com>
2022-10-30 21:48:47 +0530

    added feather-icon theme change

--- index.js
+++ index.js
@@ -45,9 +45,27 @@ function showPlace() {
   title.innerHTML = data[random];
 }
 
+var flag = 0;
+
 btn.addEventListener("click", showPlace);
 
 function handleClick(){
   var element = document.body;
   element.classList.toggle("light-mode");
+  var themebtn = document.getElementById("theme-btn");
+
+if(flag === 0){
+  themebtn.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun" style="width: 32px; height: 32px" onclick="handleClick()"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>';
+  flag = 1;
+  
+}else{
+  themebtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon" style="width: 32px; height: 32px" onclick="handleClick()"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>'
+  flag = 0;
+};
+
+  
 }
+
+
+
+feather.replace()
